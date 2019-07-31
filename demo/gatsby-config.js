158eaf06298caa72baa6b272429eaa7fbfbb09d9
sw-yx <swyx@dontemail.me>
2019-07-31 09:48:29 -0400

    add some headers?

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -23,6 +23,23 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
-    "gatsby-plugin-netlify",
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        // headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        // allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        allPageHeaders: [
+          "Link: </rss.xml>; rel=preload",
+          "Link: </sitemap.xml>; rel=preload",
+          "Link: </manifest.webmanifest>; rel=preload",
+          "Link: </index.html>; rel=preload",
+        ],
+        // mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        // mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        // mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        // transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        // generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
   ],
 }
